60ad4e719d692c2cd6dec44d88ec84949767e7f9
Stringgc <guocegz@163.com>
2016-07-09 20:24:40 +0800

    Update HelloWorld.java

--- src/com/hello/HelloWorld.java
+++ src/com/hello/HelloWorld.java
@@ -1,1 +1,15 @@
 
+package com.hello;
+
+public class HelloWorld {
+	
+		private Message message;
+		
+		public void setMessage(Message message) {
+			this.message = message;
+		}
+		
+		public void print() {
+			System.out.println("message:" + message.message);
+		}
+}
