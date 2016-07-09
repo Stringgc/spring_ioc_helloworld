41560b23af6547d450099c361149c67ba5210dc1
Stringgc <guocegz@163.com>
2016-07-09 20:25:58 +0800

    Update Test.java

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -1,1 +1,18 @@
+package com.test;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.hello.HelloWorld;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("Bean.xml");
+		
+		HelloWorld obj = (HelloWorld)context.getBean("helloworld");
+
+		obj.print();
+	}
+
+}
